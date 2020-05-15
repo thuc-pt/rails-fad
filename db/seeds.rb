68db675fca6405b725613415a4b63f0982e00672
Thuc <ptthuc77@gmail.com>
2020-05-15 19:30:06 +0700

    end fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,38 @@
 # Examples:
 # movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 # Character.create(name: 'Luke', movie: movies.first)
-User.create(
-  role_id: 1,
-  email: "ptthuc77@gmail.com",
+User.create([
+  {role_id: 1,
+  email: "thucpt@gmail.com",
   name: "Phan Trong Thuc",
   password: "123456",
-  password_confirmation: "123456"
-)
+  password_confirmation: "123456",
+  confirmed_at: Date.current},
+  {role_id: 2,
+  email: "example01@gmail.com",
+  name: "Example 01",
+  password: "123456",
+  password_confirmation: "123456",
+  confirmed_at: Date.current},
+  {role_id: 2,
+  email: "example02@gmail.com",
+  name: "Example 02",
+  password: "123456",
+  password_confirmation: "123456",
+  confirmed_at: Date.current},
+  {role_id: 2,
+  email: "example03@gmail.com",
+  name: "Example 03",
+  password: "123456",
+  password_confirmation: "123456",
+  confirmed_at: Date.current},
+  {role_id: 2,
+  email: "example04@gmail.com",
+  name: "Example 04",
+  password: "123456",
+  password_confirmation: "123456",
+  confirmed_at: Date.current}
+])
 
 Payment.create([
   {name: "Payment on delivery"},
